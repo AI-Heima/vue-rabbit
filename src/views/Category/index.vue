2236bab99e45b11d863c0ba9cb04694de0024956
itheima <itheima@itcast.cn>
2024-06-18 17:16:25 +0800

    home-轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { getCategoryAPI } from '@/apis/category'
-import { onMounted, ref } from 'vue'
+import { onBeforeUpdate, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { getBannerAPI } from '@/apis/home';
 
 // 获取数据
 const categoryData = ref({})
@@ -10,8 +11,21 @@ const getCategory = async () => {
   const res = await getCategoryAPI(route.params.id)
   categoryData.value = res.result
 }
-
 onMounted(() => getCategory())
+
+onBeforeUpdate(() => getCategory())
+
+// 获取banner
+const bannerList = ref([])
+
+const getBanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite: '2'
+  })
+  bannerList.value = res.result
+}
+
+onMounted(() => getBanner())
 
 </script>
 
@@ -24,6 +38,14 @@ onMounted(() => getCategory())
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
+      </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
+        </el-carousel>
       </div>
     </div>
   </div>
@@ -106,6 +128,17 @@ onMounted(() => getCategory())
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
