88c5e11ec500eb6bf3cafd829528fd355afb07cc
itheima <itheima@itcast.cn>
2024-06-19 16:47:57 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/stores/user'
-import 'element-plus/theme-chalk/el-message.css'
+import router from '@/router'
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
@@ -24,11 +24,19 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
-  // 统一错误
+  const userStore = useUserStore()
+  // 统一错误提示
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+  // 401token生效处理
+  // 1.清除本地用户数据
+  // 2.跳转到登录页
+  if (e.response.status === 401) {
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
