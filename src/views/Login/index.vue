e0a1231a39293ba67b4103f709140ad6d846168d
itheima <itheima@itcast.cn>
2024-06-19 14:34:28 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -7,7 +7,8 @@ import { ref } from 'vue'
 // 1.准备表单对象
 const form = ref({
   account: '',
-  password: ''
+  password: '',
+  agree: true
 })
 
 // 2.准备规则对象
@@ -18,6 +19,19 @@ const rules = {
   password: [
     { required: true, message: '密码不能为空', trigger: 'blur' },
     { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' }
+  ],
+  agree: [
+    {
+      validator: (rule, value, callback) => {
+        // 自定义校验逻辑
+        // 勾选就通过 不勾选就不通过
+        if (value) {
+          callback()
+        } else {
+          callback(new Error('请勾选协议'))
+        }
+      }
+    }
   ]
 }
 
@@ -55,8 +69,8 @@ const rules = {
               <el-form-item label="密码" prop="password">
                 <el-input v-model="form.password" type="password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item label-width="22px" prop="agree">
+                <el-checkbox  size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
