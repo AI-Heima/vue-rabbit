a534b3f394704bbcadca31b7362573a9969286e4
AI-Heima <595504667@qq.com>
2024-06-20 15:52:06 +0800

    本地购物车-购物车列表-全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -31,25 +31,37 @@ export const useCartStore = defineStore('cart', () => {
     cartList.value.splice(idx, 1)
   }
 
-  // 计算属性
-  // 1.总的数量 所有项的count之和
-  const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
-  // 2.总价 所有项的count*price之和
-  const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
-  
   // 单选功能
   const singleCheck = (skuId, selected) => {
     // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
     const item = cartList.value.find((item) => item.skuId === skuId)
     item.selected = selected
   }
+
+  // 全选功能
+  const allCheck = (selected) => {
+    // 把cartList中的每一项的selected都设置为当前的全选框状态
+    cartList.value.forEach(item => item.selected = selected)
+  }
+
+  // 计算属性
+  // 1.总的数量 所有项的count之和
+  const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+  // 2.总价 所有项的count*price之和
+  const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+  
+  // 是否全选
+  const isAll = computed(() => cartList.value.every((item) => item.selected))
+  
   return {
     cartList,
-    addCart,
-    delCart,
     allCount,
     allPrice,
-    singleCheck
+    isAll,
+    addCart,
+    delCart,
+    singleCheck,
+    allCheck
   }
 }, {
   persist: true // 开启本地存储
